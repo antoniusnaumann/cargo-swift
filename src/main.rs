87cbb712a4823d9fa1e0b8dc3713bcb3726aff4b
Antonius Naumann <git@antonius.dev>
2023-01-19 19:08:12 +0100

    Add struct for command args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    action: Action,
+}
+
+#[derive(Subcommand, Debug)]
+enum Action {
+    Init,
+    Package,
+}
+
 fn main() {
     println!("Hello, world!");
 }
