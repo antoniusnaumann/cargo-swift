a3582df1c1de9831c3489f9e6cef4c2dd0977dfb
Antonius Naumann <git@antonius.dev>
2023-03-18 11:41:03 +0100

    Show failed step when creating project files fail in init

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -33,8 +33,6 @@ pub fn run(crate_name: String, config: Config, vcs: Vcs) -> Result<()> {
     ));
     spinner.start();
 
-    create_dir(&crate_name).expect("Could not create directory for crate!");
-
     let manifest = Manifest::from_str(include_str!("../../Cargo.toml")).unwrap();
     let cargo_swift_version = manifest.package().version();
 
@@ -45,15 +43,16 @@ pub fn run(crate_name: String, config: Config, vcs: Vcs) -> Result<()> {
     let build_rs_content = include_str!("../../template/template.build.rs");
     let udl_content = include_str!("../../template/template.lib.udl");
 
-    write(format!("{}/Cargo.toml", crate_name), cargo_toml_content)
-        .map_err(|_| "Could not write Cargo.toml!")?;
-    write(format!("{}/build.rs", crate_name), build_rs_content).expect("Could not write build.rs!");
-
-    create_dir(format!("{}/src", crate_name)).expect("Could not create src/ directory!");
-    write(format!("{}/src/lib.rs", crate_name), lib_rs_content)
-        .map_err(|_| "Could not write src/lib.rs!")?;
-    write(format!("{}/src/lib.udl", crate_name), udl_content)
-        .map_err(|_| "Could not write src/lib.udl!")?;
+    if let Err(e) = write_project_files(
+        &cargo_toml_content,
+        build_rs_content,
+        &lib_rs_content,
+        udl_content,
+        &crate_name,
+    ) {
+        spinner.fail();
+        Err(e)?
+    }
 
     spinner.finish();
 
@@ -61,6 +60,28 @@ pub fn run(crate_name: String, config: Config, vcs: Vcs) -> Result<()> {
         Vcs::Git => init_git_repository(&crate_name, config.silent)?,
         Vcs::None => (),
     };
+
+    Ok(())
+}
+
+fn write_project_files(
+    cargo_toml: &str,
+    build_rs: &str,
+    lib_rs: &str,
+    lib_udl: &str,
+    crate_name: &str,
+) -> Result<()> {
+    create_dir(crate_name).map_err(|_| "Could not create directory for crate!")?;
+
+    write(format!("{}/Cargo.toml", crate_name), cargo_toml)
+        .map_err(|_| "Could not write Cargo.toml!")?;
+    write(format!("{}/build.rs", crate_name), build_rs).expect("Could not write build.rs!");
+
+    create_dir(format!("{}/src", crate_name)).expect("Could not create src/ directory!");
+    write(format!("{}/src/lib.rs", crate_name), lib_rs)
+        .map_err(|_| "Could not write src/lib.rs!")?;
+    write(format!("{}/src/lib.udl", crate_name), lib_udl)
+        .map_err(|_| "Could not write src/lib.udl!")?;
 
     Ok(())
 }
