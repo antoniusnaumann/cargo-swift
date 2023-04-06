e7dbb5b0a83734865ebd1858cc9ca0d5398b0c5a
Antonius Naumann <git@antonius.dev>
2023-04-06 21:59:16 +0200

    Add directory for example xcodeprojects

---
+++ swift-examples/hello-world/HelloWorld/HelloWorld/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by Antonius Naumann on 06.04.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
