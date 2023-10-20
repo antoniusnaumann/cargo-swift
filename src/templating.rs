808ea4988fb7a0964be8704701434b34650d60ab
Antonius Naumann <git@antonius.dev>
2023-10-20 14:58:04 +0200

    Rename Cargo.toml to template.toml to not implicitly exclude templates/ directory from published package

--- src/templating.rs
+++ src/templating.rs
@@ -1,7 +1,7 @@
 use askama::Template;
 
 #[derive(Template)]
-#[template(path = "Cargo.toml", escape = "none")]
+#[template(path = "template.toml", escape = "none")]
 pub(crate) struct CargoToml<'a> {
     pub(crate) crate_name: &'a str,
     pub(crate) namespace: &'a str,
