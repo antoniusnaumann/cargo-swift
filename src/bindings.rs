4ef8b2c096fd3f22d319fd6360d0a6ee707a1e19
Andrej Mihajlov <andrej@nymtech.net>
2026-01-16 14:37:38 +0100

    Migrate to uniffi-bindgen 0.31

--- src/bindings.rs
+++ src/bindings.rs
@@ -1,11 +1,11 @@
 use std::{
     fs::{self, create_dir},
-    io,
+    io::{self, Write},
 };
 
-use crate::{metadata::metadata, Result};
+use crate::Result;
 use camino::Utf8Path;
-use uniffi_bindgen::{bindings::SwiftBindingGenerator, cargo_metadata::CrateConfigSupplier};
+use uniffi_bindgen::bindings::{GenerateOptions, TargetLanguage};
 
 use crate::recreate_dir;
 
@@ -19,38 +19,47 @@ pub fn generate_bindings(lib_path: &Utf8Path) -> Result<()> {
     create_dir(&headers)?;
     create_dir(&sources)?;
 
-    let uniffi_outputs = uniffi_bindgen::library_mode::generate_bindings(
-        lib_path,
-        None,
-        &SwiftBindingGenerator {},
-        &CrateConfigSupplier::from(metadata().clone()),
-        None,
-        out_dir,
-        false,
-    )?;
+    let options = GenerateOptions {
+        languages: vec![TargetLanguage::Swift],
+        source: lib_path.to_path_buf(),
+        out_dir: out_dir.to_path_buf(),
+        metadata_no_deps: true,
+        ..Default::default()
+    };
+    uniffi_bindgen::bindings::generate(options)?;
 
     let mut modulemap = fs::OpenOptions::new()
         .create(true)
         .append(true)
         .open(headers.join("module.modulemap"))?;
 
-    for output in uniffi_outputs {
-        let namespace = output.ci.namespace();
-        fs::copy(
-            out_dir.join(format!("{namespace}.swift")),
-            sources.join(format!("{namespace}.swift")),
-        )?;
+    let dir = fs::read_dir(out_dir)?;
 
-        let ffi_name = format!("{namespace}FFI");
-        fs::copy(
-            out_dir.join(format!("{ffi_name}.h")),
-            headers.join(format!("{ffi_name}.h")),
-        )?;
+    for f in dir {
+        let f = f?;
+        let file_path = f.path();
 
-        let mut modulemap_part = fs::OpenOptions::new()
-            .read(true)
-            .open(out_dir.join(format!("{ffi_name}.modulemap")))?;
-        io::copy(&mut modulemap_part, &mut modulemap)?;
+        if !f.metadata()?.is_file() {
+            continue;
+        }
+
+        let Some(name) = file_path.file_name() else {
+            continue;
+        };
+
+        let Some(ext) = file_path.extension() else {
+            continue;
+        };
+
+        if ext == "swift" {
+            fs::copy(out_dir.join_os(name), sources.join_os(name))?;
+        } else if ext == "h" {
+            fs::copy(out_dir.join_os(name), headers.join_os(name))?;
+        } else if ext == "modulemap" {
+            let mut modulemap_part = fs::OpenOptions::new().read(true).open(file_path)?;
+            io::copy(&mut modulemap_part, &mut modulemap)?;
+            modulemap.write_all(b"\n")?;
+        }
     }
 
     Ok(())
